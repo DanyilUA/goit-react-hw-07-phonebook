5d3314bafa66e008f397db44649fb2e9f506d2e7
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-09-02 12:53:56 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import AppPhoneBook from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
@@ -10,10 +9,8 @@ import { store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-07-phonebook">
       <Provider store={store}>
           <AppPhoneBook />
       </Provider>
-    </BrowserRouter>
   </React.StrictMode>
 );
